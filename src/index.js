9a8a9b78e0340a254b6a5691c84ac571388c4273
Jokeseph <josephofilii@gmail.com>
2022-05-05 16:58:13 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 // import Popper from '@popperjs/core';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 ReactDOM.render(
